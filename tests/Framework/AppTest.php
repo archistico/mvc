4638286bfb07e611c91966e2ca86460871a8192c
archistico <info @archistico.com>
2017-11-09 12:13:24 +0100

    test errore 404 failed

--- tests/Framework/AppTest.php
+++ tests/Framework/AppTest.php
@@ -21,4 +21,12 @@ class AppTest extends TestCase {
         $this->assertContains('<h1>Blog</h1>', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
     }
+
+    public function testError404() {
+        $app = new App();
+        $request = new ServerRequest('GET','/aze');
+        $response = $app->run($request);
+        $this->assertContains('<h1>Error 404</h1>', (string)$response->getBody());
+        $this->assertEquals(404, $response->getStatusCode());
+    }
 }
